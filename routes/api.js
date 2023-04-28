6c6a05ca38c501e9a2a470a1fc241832bb5cb80b
Tjcarroll1 <tjcarroll1@icloud.com>
2023-04-28 15:29:42 +1000

    added post request in api.js

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,22 @@ const router = require('express').Router();
 const { readFromFile, writeToFile, readAndAppend } = require('../helper/utils.js');
 const uuid = require('../helper/uuid.js')
 
+// Gets the users notes typed on the html and saves into the db.json i.e it's saving the data
 router.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-
+// Posts the users data into the saved side on the left
+router.post('/notes', (req, res) => {
+  const dbJson = JSON.parse(fs.readFromFile('db/db.json'));
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text;
+    id: uuid,
+  }
+  dbJson.push(newNote);
+  fs.writeToFile('db/db.json',JSON.stringify(dbJson))
+  res.json(dbJson)
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const htmlRoute = require('./routes/html')
 const apiRoutes = require('./routes/api')
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+
+// Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 app.use('/api', apiRoutes);
 app.use('/', htmlRoute);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
